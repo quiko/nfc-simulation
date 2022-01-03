977ce6092b34227821f350a376d3065a6bf16b0d
Hanaa <houlad@wemanity.com>
2022-01-03 10:20:49 +0100

    add constants

---
+++ constants.js
@@ -0,0 +1,7 @@
+const loginUrl = "https://api.staging.genexir.selinko.com/api/v2/sessions";
+const scanUrl = "https://api.staging.genexir.selinko.com/api/v2/authentication";
+const apiKey = "pk_0UXcXJRPNqXPe8qaVVl2";
+const shortUrl ="https://api.staging.genexir.selinko.com/_g6XQ0fN07tD-95y";
+
+
+export  {loginUrl, scanUrl, apiKey, shortUrl}
